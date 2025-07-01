d182e72651dff2f688a29f0e62578647a5ee5142
taykim7 <th_77@naver.com>
2025-07-01 22:43:16 +0900

    StyleSheet 활용 스타일링

--- react-native-basic/AwesomeProject/screens/MainScreen.js
+++ react-native-basic/AwesomeProject/screens/MainScreen.js
@@ -1,20 +1,49 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 
 export default function MainScreen() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <StatusBar barStyle={'default'} />
-      <Text>투두앱</Text>
-      <View>
-        <Text>해야할 일</Text>
+      <Text style={styles.pageTitle}>투두앱</Text>
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>해야할 일</Text>
       </View>
-      <View>
-        <Text>완료된 일</Text>
+      <View style={styles.separator} />
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>완료된 일</Text>
       </View>
     </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? 20 : 0,
+    backgroundColor: '#f7f8fa',
+  },
+  pageTitle: {
+    marginBottom: 35,
+    paddingHorizontal: 15,
+    fontSize: 54,
+    fontWeight: '600'
+  },
+  separator: {
+    marginHorizontal: 10,
+    marginTop: 25,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(0, 0, 0, 0.2)'
+  },
+  listView: {
+    flex: 1,
+  },
+  listTitle: {
+    marginBottom: 25,
+    paddingHorizontal: 15,
+    fontSize: 41,
+    fontWeight: '500',
+  }
+})
