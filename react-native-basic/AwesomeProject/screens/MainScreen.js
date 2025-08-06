e06c15fe7051684398ae7b9509027aa6c155e1bc
taykim7 <th_77@naver.com>
2025-08-06 22:02:36 +0900

    useSelector을 통해 데이터 가져오기
    - 데이터 가져온 후 데이터가 있을 경우 FlatList를 통해 뿌리기
    - 데이터가 없으면 그냥 텍스트

--- react-native-basic/AwesomeProject/screens/MainScreen.js
+++ react-native-basic/AwesomeProject/screens/MainScreen.js
@@ -1,21 +1,42 @@
-import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import InputForm from '../components/InputForm';
 import TodoItem from '../components/TodoItem';
+import { useSelector } from 'react-redux';
 
 export default function MainScreen() {
+  const todos = useSelector(state => state.todo.todos);
+  const todoTasks = todos.filter((item) => item.state === 'todo');
+  const completedTasks =  todos.filter((item) => item.state === 'done');
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle={'default'} />
       <Text style={styles.pageTitle}>투두앱</Text>
       <View style={styles.listView}>
         <Text style={styles.listTitle}>해야할 일</Text>
-        <TodoItem />
+        {todoTasks.length !== 0 ? (
+          <FlatList
+            data={todoTasks}
+            renderItem={({item}) => <TodoItem {...item} />}
+            keyExtractor={(item) => item.id}
+          />
+        ) :
+          (<Text style={styles.emptyListText}>할 일이 없습니다.</Text>)
+        }
       </View>
       <View style={styles.separator} />
       <View style={styles.listView}>
         <Text style={styles.listTitle}>완료된 일</Text>
+        {completedTasks.length !== 0 ? (
+          <FlatList
+            data={completedTasks}
+            renderItem={({item}) => <TodoItem {...item} />}
+            keyExtractor={(item) => item.id }
+          />
+        ) :
+        (<Text></Text>)}
       </View>
       <InputForm />
     </SafeAreaView>
@@ -49,5 +70,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     fontSize: 41,
     fontWeight: '500',
-  }
+  },
+  emptyListText: {}
 })
